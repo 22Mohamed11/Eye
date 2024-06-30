cb1b0f0b5ed4a1e05674b93155ea38d76d373442
m0hamedKhalidMG <mk9636736@gmail.com>
2024-07-01 02:17:58 +0300

    solve debug

--- eye.py
+++ eye.py
@@ -1,21 +1,24 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 from PIL import Image
 from flask import Flask, request, jsonify
-from tensorflow.keras.preprocessing import image # type: ignore
-from tensorflow.keras.applications.vgg16 import preprocess_input # type: ignore
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
 import numpy as np
 import keras
 
 app = Flask(__name__)
 
-model = keras.models.load_model('eyee.keras')
+model = keras.models.load_model('eyee.keras')  # Ensure this path is correct
 
 # Define class labels
 class_labels = ['Uveitis', 'Normal_Eyee']
 
 def preprocess_image(img):
-    img = img.resize((150, 150))  
-    img_array = image.img_to_array(img)  
-    img_array = np.expand_dims(img_array, axis=0) 
+    img = img.resize((150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
     return img_array
 
@@ -25,7 +28,7 @@ def predict():
         return jsonify({'error': 'No file part'})
     file = request.files['file']
     if file.filename == '':
-       return jsonify({'error': 'No selected file'})
+        return jsonify({'error': 'No selected file'})
 
     if file:
         try:
@@ -47,4 +50,4 @@ def predict():
             return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True) #host = "0.0.0.0",port = 8000,
+    app.run(host="0.0.0.0", port=8000, debug=True)
